7276e0079e51f41a26b1a9ef41fdfbf2142a0915
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-06-21 19:22:00 +0200

    ADD LOADING BAR

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import {getInitialData} from '../utils/api'
 import {receiveUsers,} from './users'
 import {receiveTweets,} from './tweets'
 import {setAuthedUser} from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
 const AUTHED_ID='tylermcginnis';
 
 //handle async request to get initial data from the API and add data to the store 
 export function handleInitialData(){
     return(dispatch)=>{
+        dispatch(showLoading())
         return getInitialData().then(({ users,tweets,})=>{
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
 
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import authedUser from '../reducers/authedUser'
 import users from '../reducers/users'
 import tweets from '../reducers/tweets'
 import {combineReducers} from 'redux'
+import { loadingBarReducer } from 'react-redux-loading-bar'
+
+
 
 
 export default combineReducers({
     authedUser: authedUser,
     tweets: tweets,
-    users: users
+    users: users,
+    loadingBar: loadingBarReducer,
   });
